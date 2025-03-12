65c851564a2737f98af9ef4f3178d691de3d908b
Macbook <macbook@Macbooks-MBP.mshome.net>
2025-03-12 21:30:11 +0700

    Login, Register, Logout, Google auth,  Reset Password

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Container\Attributes\Auth;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+
+    Route::post('/request-password-reset', [AuthController::class, 'requestPasswordReset']);
+    Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
+});
+
+
+
