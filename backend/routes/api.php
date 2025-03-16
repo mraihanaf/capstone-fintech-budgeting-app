0940af69ad03bd7e07dc4653c1339a43f6702b43
Arfa Banyu Santoro <banyuarfa1122@gmail.com>
2025-03-16 08:32:50 +0700

    refactor: add password and profile routes grouping, and change the route names

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,31 +3,37 @@
 use App\Http\Controllers\BudgetController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LogController;
+use App\Http\Controllers\PasswordController;
 use App\Http\Controllers\RecommendationController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\TargetController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
 
-    Route::post('/request-password-reset', [AuthController::class, 'requestPasswordReset']);
-    Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
-
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
-        Route::get('user', [AuthController::class, 'user']);
+        Route::get('user', [UserController::class, 'show']);
     });
 });
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/update-profil', [UserController::class, 'UpdateProfile']);
-    Route::post('/change-password', [UserController::class, 'ChangePassword']);
+Route::prefix('password')->group(function () {
+    Route::post('forgot', [PasswordController::class, 'forgot']);
+    Route::post('reset', [PasswordController::class, 'reset']);
+    Route::post('change', [PasswordController::class, 'change'])->middleware('auth:sanctum');
+});
 
+Route::prefix('profile')->group(function () {
+    Route::post('/update-profil', [ProfileController::class, 'update'])->middleware('auth:sanctum');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('transactions', TransactionController::class);
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('budgets', BudgetController::class);
