f76392d42ac0250ceaab3b72c91f3802f40e84f6
Arfa Banyu Santoro <banyuarfa1122@gmail.com>
2025-03-19 20:27:03 +0700

    style(spacing): format add spacing

--- backend/database/migrations/2025_03_10_102337_create_logs_table.php
+++ backend/database/migrations/2025_03_10_102337_create_logs_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('action');
+            $table->date('log_date');
             $table->text('details')->nullable();
             $table->timestamps();
         });
