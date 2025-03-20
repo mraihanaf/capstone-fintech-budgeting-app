fe5de9b35627d2ae9ad81bb9220609c9e9c421ac
Arfa Banyu Santoro <banyuarfa1122@gmail.com>
2025-03-20 19:12:43 +0700

    feat(table): remove log_date column

--- backend/database/migrations/2025_03_10_102337_create_logs_table.php
+++ backend/database/migrations/2025_03_10_102337_create_logs_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('action');
-            $table->date('log_date');
             $table->text('details')->nullable();
             $table->timestamps();
         });
